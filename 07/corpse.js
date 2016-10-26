11895309a63abe82dfc21ffd351b6f4e3d1242f9
Noah Paessel <knowuh@gmail.com>
2016-10-26 06:29:51 -0400

    Updated the corpse player.

--- 07/corpse.js
+++ 07/corpse.js
@@ -1,78 +1,63 @@
 var myCanvas = document.getElementById('corpse');
 var drawingPad = myCanvas.getContext('2d');
-var scripts = [];
-var knowuh = {};
+var studentFunctions = [];
 
-// possibly change drawImage(filename, ctx, x, y)
-var makeDrawImage = function(username) {
-  var originalDrawImage = window[username].drawImage;
-  if (originalDrawImage) {
-    console.log("Changing draw image function");
-    var newFunction = function (filename, ctx, x, y) {
-      if (filename.split(".").length > 2) {
-        filename = "//" + name + ".github.io/2016-SWC/ps-06" + filename;
-      }
-      originalDrawImage(filename, ctx, x, y);
-    };
-    window[username].drawImage = newFunction;
-  }
-  else {
-    console.log("no draw image for " + username);
-  }
-};
-
-var addScript = function (student, path) {
-  var name = student.github;
-  var webPage = "//" + name + ".github.io/2016-SWC/" + path;
-  jQuery.getScript(webPage, function(script) {
-    console.log("Added script for " + name);
-    scripts.push(name);
-    makeDrawImage(name);
-  });
-};
-
-var loadCorpses = function(problemset) {
-  $.getJSON( "../roster.json", function(students) {
-    $('#dynamic-content').append($("<ul className='solutions' id='solutions'>"));
-    for (var student of students) {
-      addScript(student, "ps-06/corpse.js");
+var addStudentFunction = function(username) {
+  var scriptUrl = "//" + username + ".github.io/2016-SWC/ps-06/corpse.js";
+  jQuery.getScript(scriptUrl, function(script) {
+    console.log("Added script for " + username);
+    if (window[username] && window[username].drawTop) {
+      studentFunctions.push(username);
     }
   });
 };
 
+// Read github usernames from roster.json.
+// Then load into the document each students corpse.js file.
+// expects that each student defined window.githubUsername
+var loadCorpses = function() {
+  $.getJSON( "../roster.json", function(students) {
+    console.dir(students);
+    for (var i=0; i < students.length; i++) {
+      addStudentFunction(students[i].github);
+    }
+  });
+};
 
 var choose = function() {
-  var index = Math.random() * scripts.length;
-  return scripts[index -1];
+  var index = Math.floor(Math.random() * studentFunctions.length);
+  var student = studentFunctions[index];
+  console.log(student)
+  return student;
 }
 
-knowuh.drawTop = function(ctx) {
+
+var drawTop = function(ctx) {
   var user=choose();
   window[user].drawTop(ctx);
 };
 
-knowuh.drawMiddle = function(ctx) {
+var drawMiddle = function(ctx) {
   var user=choose();
   window[user].drawMiddle(ctx);
 };
 
-knowuh.drawBottom = function(ctx) {
+var drawBottom = function(ctx) {
   var user=choose();
-  window[user].drawMiddle(ctx);
+  window[user].drawBottom(ctx);
 };
 
-knowuh.drawCorpse = function() {
-  if (scripts.length > 0) {
-    console.dir(scripts);
-    knowuh.drawTop(drawingPad);
-    knowuh.drawMiddle(drawingPad);
-    knowuh.drawBottom(drawingPad);
-  }
-  else {
-    console.log("Nothing here...");
+var drawCorpse = function() {
+  if (studentFunctions.length > 0) {
+    drawingPad.fillStyle = 'white';
+    drawingPad.fillRect(0, 0, 400, 720);
+    drawTop(drawingPad);
+    drawMiddle(drawingPad);
+    drawBottom(drawingPad);
   }
 };
 
 loadCorpses();
-var interval = 5 * 1000;
-setInterval(knowuh.drawCorpse, interval);
+
+var interval = 1 * 1000;
+setInterval(drawCorpse, interval);
