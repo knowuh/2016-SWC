cf6af0910847768adabd0fdf7f0dd3661928da98
Noah Paessel <knowuh@gmail.com>
2016-11-23 09:52:33 -0500

    Clean up mapping example.

--- 11/d3-geo.js
+++ 11/d3-geo.js
@@ -5,43 +5,57 @@ var svg = d3.select("#map").append("svg")
   .attr("width", width)
   .attr("height", height);
 
-var projection = d3.geoMercator().scale(1).translate([0,0]);
+var projection;
 
-d3.json("simple-ma.json", function(data) {
+
+// Setup our project to fit all the complete map into our SVG region:
+function fitMapToSVGBounds(geoData) {
+  projection = d3.geoMercator().scale(1).translate([0,0]);
   var path = d3.geoPath().projection(projection);
-  var bounds = path.bounds(data);
+  var bounds = path.bounds(geoData);
   var scale = .95 / Math.max((bounds[1][0] - bounds[0][0]) / width,
       (bounds[1][1] - bounds[0][1]) / height);
   var transl = [(width - scale * (bounds[1][0] + bounds[0][0])) / 2,
     (height - scale * (bounds[1][1] + bounds[0][1])) / 2];
-
   projection.scale(scale).translate(transl);
+};
 
+// Draw our base map, which can be something made here: http://mapstarter.com/
+function drawBaseMap(geoData) {
+  var path = d3.geoPath().projection(projection);
   svg.selectAll(".land")
-    .data(data.features)
+    .data(geoData.features)
     .enter().append('path')
     .attr('class','land')
-    .style("fill", "hsl(0,10%,60%")
+    .style("fill", "hsl(0,10%,30%")
     .attr("d", path);
-});
+}
 
-d3.json("./noahs-geo.json", function(data) {
+// Load a custom point data layer created with http://geojson.io/
+function loadAndDrawPointLayer(geoJasonFile){
+  d3.json(geoJasonFile, function(data) {
+    svg.selectAll(".places")
+      .data(data.features)
+      .enter().append('circle')
+      .attr('class','places')
+      .style("fill", "white")
+      .attr("r", 4)
+      .attr("cx", function(data) {
+        return projection(data.geometry.coordinates)[0];
+      })
+      .attr("cy", function(data) {
+        return projection(data.geometry.coordinates)[1];
+      } );
+  });
+};
 
-  svg.selectAll(".places")
-    .data(data.features)
-    .enter().append('circle')
-    .attr('class','places')
-    .style("fill", "black")
-    .attr("r", 4)
-    .attr("cx", function(data) {
-      var x = projection(data.geometry.coordinates)[0];
-      return x;
-    })
-    .attr("cy", function(data) {
-      var y = projection(data.geometry.coordinates)[1];
-      console.log(y);
-      return y;
-    } );
+d3.json("simple-ma.json", function(geoData) {
+  // 1: setup map projection:
+  fitMapToSVGBounds(geoData);
+  // 2: draw the base map (usually outlines):
+  drawBaseMap(geoData);
+  // 3: draw point data:
+  loadAndDrawPointLayer('./noahs-geo.json');
 });
 
 
